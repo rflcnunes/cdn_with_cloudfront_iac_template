3836dcdd55fba61eec74c9f464ef2c09c1b01b7f
Rafael da Costa Nunes <rafael.nunes@appmax.com.br>
2024-07-13 01:20:08 -0300

    add iam module with lambda roles

---
+++ modules/iam/main.tf
@@ -0,0 +1,9 @@
+resource "aws_iam_role" "lambda_role" {
+  name               = "${var.environment}-lambda-role"
+  assume_role_policy = data.aws_iam_policy_document.lambda_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  role       = aws_iam_role.lambda_role.name
+}
---
+++ modules/iam/variables.tf
@@ -0,0 +1,4 @@
+variable "environment" {
+  type        = string
+  description = "Environment where the resources will be created"
+}
