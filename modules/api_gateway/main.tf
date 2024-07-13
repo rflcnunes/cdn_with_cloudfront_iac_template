4516fd3a8039dc9fe400288aed2576a48d011d2a
Rafael da Costa Nunes <rafael.nunes@appmax.com.br>
2024-07-13 16:09:10 -0300

    adjustment in permission to lambda and api gateway

--- modules/api_gateway/main.tf
+++ modules/api_gateway/main.tf
@@ -9,13 +9,15 @@ resource "aws_lambda_permission" "apigw_lambda" {
   function_name = var.lambda_function_name
   principal     = "apigateway.amazonaws.com"
 
-  source_arn = aws_api_gateway_rest_api.example_cdn_api.execution_arn
+  source_arn = "${aws_api_gateway_rest_api.example_cdn_api.execution_arn}/*/*"
 }
 
 resource "aws_api_gateway_deployment" "example_cdn_api" {
   rest_api_id = aws_api_gateway_rest_api.example_cdn_api.id
 
-  depends_on = [aws_api_gateway_account.api_gw_account, aws_lambda_permission.apigw_lambda]
+  depends_on = [
+    aws_api_gateway_integration.ip_verifier_get_lambda
+  ]
 }
 
 resource "aws_api_gateway_stage" "example_cdn_api" {
